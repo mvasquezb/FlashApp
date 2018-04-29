e8c6933d42ca28612505f4153ecadc3db86468c7
Martin Vasquez <pedromvasquezb@gmail.com>
2018-04-28 20:35:21 -0500

    Add Fresco dependency

--- android_app/app/src/main/java/com/oligark/flashapp/FlashApp.kt
+++ android_app/app/src/main/java/com/oligark/flashapp/FlashApp.kt
@@ -1,11 +1,12 @@
 package com.oligark.flashapp
 
 import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
 
 class FlashApp : Application() {
     override fun onCreate() {
         super.onCreate()
-
+        Fresco.initialize(this)
         // TODO: Update for dependency injection
     }
 }

42e29c8644c11f81ce5355a64b92cd6e9812d4fd
a20135298@inf.pucp.edu.pe <a20135298@inf.pucp.edu.pe>
2018-05-25 17:42:45 -0500

    mockup registrar mascota

---
+++ android_app/app/src/main/java/com/oligark/flashapp/mascotas.java
@@ -0,0 +1,13 @@
+package com.oligark.flashapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mascotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mascotas);
+    }
+}

c94359b3d95849b11e352bd9c6fa902df20fdfd7
gonchi <a20135298@pucp.pe>
2018-06-24 00:07:32 +0200

    heroku server

--- android_app/app/src/main/java/com/oligark/flashapp/service/api/BaseApi.kt
+++ android_app/app/src/main/java/com/oligark/flashapp/service/api/BaseApi.kt
@@ -3,6 +3,6 @@ package com.oligark.flashapp.service.api
 object BaseApi {
     // TODO: Update when api is created
     val apiUrl: String
-        //get() = "https://flashapp-20181.herokuapp.com/api/"
-        get() = "http://10.100.241.76/FlashApp-Backend/public/api/"
+        get() = "https://flashapp-20181.herokuapp.com/api/"
+        //get() = "http://10.100.241.76/FlashApp-Backend/public/api/"
 }
